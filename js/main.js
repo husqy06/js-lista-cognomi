1d30729291ff204100eb17b66bfc0aaf7d1d60bd
husqy06 <luigf306@gmail.com>
2021-07-20 15:26:53 +0200

    Struttura javascript

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// Creo il mio array
+
+var myList = [
+    "Bianchi",
+    "Neri",
+    "Rossi",
+    "Gialli",
+    "Verdi",
+]
+
+console.log(myList);
+
+// Chiedo all'utente il cognome
+var cognomeUser;
+
+do {
+    cognomeUser = prompt("Inserisci il tuo cognome");
+} while (cognomeUser == 0)
+
+console.log(cognomeUser);
+
+// Inserisco il cognome nell' array
+
+myList.push(cognomeUser);
+
+console.log(myList);
+
+//stampo l'array ordinato
+
+for(var i = 0; i < myList.length; i++) {
+    myList.sort();
+    console.log(myList[i]);
+}
+
+// Stampo l'array sulla pagina
+  
+for (var i = 0; i < myList.length; i++) {
+    var itemList = document.createElement ("li");
+    itemList.innerHTML = myList[i];
+    document.getElementById("list").appendChild(itemList);
+}
+
+
+
